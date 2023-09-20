04906a834abe06e5b5703093fe1bffd83447e600
Oscar Jair Cuadros Nuñez <N00377634@upn.pe>
2023-09-20 12:52:16 -0500

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Ingrese el número 1:");
+int numero1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese el número 2:");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+
+int producto = 0;
+byte i = 1;
+while (i <= numero2)
+{
+    producto += numero1;
+    i++;
+}
+
+Console.WriteLine("El producto es de " + producto);
